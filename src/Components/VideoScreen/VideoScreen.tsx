a726014ac40f1ea0471519c542217545596159af
Brad <bradleymasciotra@gmail.com>
2023-12-25 13:54:20 -0800

    fade working on video screen to sub home

--- src/Components/VideoScreen/VideoScreen.tsx
+++ src/Components/VideoScreen/VideoScreen.tsx
@@ -2,28 +2,39 @@
 import "./styles.css";
 import ReactPlayer from "react-player";
 import xButton from "../../Assets/Icons/x-button.png";
+
 interface VideoScreenProps {
     setVideoComplete: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const VideoScreen: React.FC<VideoScreenProps> = ({setVideoComplete}) => {
-    
+export const VideoScreen: React.FC<VideoScreenProps> = ({ setVideoComplete }) => {
+
+
     const handleVideoEnd = () => {
+        const videoScreen = document.querySelector(".video-screen") as HTMLElement | null;
+        console.log(videoScreen);
+
         console.log("video has ended")
-        setVideoComplete(true);
+
+        videoScreen?.classList.add("fade-out")
+
+        // this is where the fade transition should happen but its not triggering
+        setTimeout(()=>{
+            document.getElementById("promo-video")?.classList.add("fade-out");
+            setVideoComplete(true);
+        },1000);
     };
-    
+
 
     return (
         <div className="video-screen">
             <div className="video-x">
-                
+
                 <div className="video-y">
-                <div id="x-button">
-                    <img className ="x-button-styles"src={xButton} onClick={handleVideoEnd}>
-                    </img>
-                    
-                </div>
+                    <div id="x-button">
+                        <img className="x-button-styles" src={xButton} onClick={handleVideoEnd}>
+                        </img>
+                    </div>
                     <div className="video">
                         <ReactPlayer
                             url={require("../../Assets/Video/sample.mp4")}
